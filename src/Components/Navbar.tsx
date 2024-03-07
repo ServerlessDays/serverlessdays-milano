6e07d9e71e8e5c61ac942478eea847f2525f1972
Federico Tensi <federicotensi@gmail.com>
2024-03-07 20:24:58 +0100

    Initial commit

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+	return (
+		<nav className="flex">
+			<div className="flex-1">Home</div>
+			<div className="flex-1">About</div>
+			<div className="flex-1">Contact</div>
+		</nav>
+	);
+};
+
+export default Navbar;
