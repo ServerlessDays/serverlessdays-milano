f7e4b560ed41ff8edfdf10db5b4bb20bc7dad485
Federico Tensi <federicotensi@gmail.com>
2024-03-14 23:04:05 +0100

    fix: texts, spacing and formatting

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -15,27 +15,27 @@ const Home = () => {
 					<h1 className="text-5xl font-extrabold md:text-6xl">
 						ServerlessDays <br /> Milano 🇮🇹
 					</h1>
-					<h3 className="text-3xl font-semibold md:text-4xl text-secondary">
+					<h3 className="text-2xl font-semibold md:text-4xl text-secondary max-w-64 sm:max-w-full">
 						<a href="Serverless_Day_2024.ics">June, 13th 2024</a> <br className="block sm:hidden" />
-						&nbsp;@&nbsp;
+						<span className="hidden md:inline-block">&nbsp;</span>@&nbsp;
 						<a href="https://maps.app.goo.gl/WYFXN32VHEQrjctJ8" target="_blank">
-							Milano C30
+							C30, Viale Cassala, Milano
 						</a>
 					</h3>
 					<TextGenerateEffect
 						words={"Join the biggest serverless community conference in Italy."}
-						containerClassName="font-medium  w-72  text-secondary sm:w-full"
-						textClassName="text-lg md:text-2xl text-secondary"
+						containerClassName="  w-80   sm:w-full md:pb-5 pb-2"
+						textClassName="text-lg font-normal md:text-2xl text-gray-200"
 					/>
 
-					<motion.div>
+					<motion.div className="">
 						<a
 							href="https://www.eventbrite.it/e/biglietti-serverlessdays-milano-2024-788514818047"
 							target="_blank"
-							className="relative"
+							className="relative "
 						>
 							<MotionButton
-								className="font-semibold text-white rounded-xl"
+								className="w-40 font-semibold text-white rounded-xl"
 								animate={{
 									//jump with spring every 2 seconds
 									y: [0, -5, 0],
@@ -46,16 +46,18 @@ const Home = () => {
 									},
 								}}
 							>
-								🎫 Buy Ticket!
+								🎫 Get your ticket!
 								{/* <span className="absolute rotate-[20deg] translate-x-14 rounded-xl bg-inherit p-1 -translate-y-4 underline">
 									35$
 								</span> */}
 							</MotionButton>
+							<div className="absolute pr-2 text-md top-8 handwriting-1">* lunch and coffee included!</div>
 							<EarlyBirdText untilDate="19/04/2024" />
 						</a>
 					</motion.div>
+					{/* <p className="handwriting-1">(Lunch and coffee included!)</p> */}
 				</div>
-				<div className="mt-24 md:mt-16 lg:mt-0">
+				<div className="pb-2 mt-24 md:pb-5 md:mt-16 lg:mt-0">
 					<img src={UnicornMascot} alt="unicorn" className="hidden md:block w-48 md:w-72 scale-x-[-1] " />
 
 					<img
@@ -84,7 +86,7 @@ const EarlyBirdText = ({ untilDate }: { untilDate: UntilTime }) => {
 	if (currentDate > until) return null;
 	return (
 		<div>
-			<div className="absolute w-20 scale-50 -bottom-12 -right-16">
+			<div className="absolute w-20 scale-50 -bottom-12 right-[-4.5rem]">
 				<img
 					src={arrowHandDrawn}
 					alt="hand drawn arrow"
@@ -92,12 +94,12 @@ const EarlyBirdText = ({ untilDate }: { untilDate: UntilTime }) => {
 				/>
 			</div>
 
-			<p className="absolute w-20 text-sm font-bold top-12 -right-24 handwriting-1">
-				Early Bird Ticket Now!{" "}
-				<span className="relative after:border-b after:rounded-[50%/100px_100px_0_0] after:border-white after:absolute after:top-4 after:-left-1 after:w-14 after:h-[1px]  after:-rotate-[3deg] after:content-['']">
+			<div className="absolute text-sm font-bold w-36 top-12 -right-44 handwriting-1">
+				Early bird tickets starting &nbsp;
+				<span className="relative inline-block after:border-b after:rounded-[50%/100px_100px_0_0] after:border-white after:absolute after:top-4 after:-left-1 after:w-14 after:h-[1px]  after:-rotate-[3deg] after:content-['']">
 					at 35€
 				</span>
-			</p>
+			</div>
 		</div>
 	);
 };
