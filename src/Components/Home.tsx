10c0225ab07081b2966a8155dd9f9c9a3223648f
Federico Tensi <federicotensi@gmail.com>
2024-03-14 13:43:03 +0100

    feat: jumping button and early bird text

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,7 +2,9 @@ import { motion } from "framer-motion";
 import UnicornMascot from "../assets/unicorn_mascot.svg";
 import { Button } from "./ui/button";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
+import arrowHandDrawn from "../assets/handDrawnArrow.png";
 const Home = () => {
+	const MotionButton = motion(Button);
 	return (
 		<div className="flex h-full p-10 text-white ">
 			<div className="w-full h-full my-auto md:items-center md:flex md:justify-around">
@@ -22,23 +24,42 @@ const Home = () => {
 						className="text-2xl font-medium text-white text-secondary"
 					/>
 
-					<motion.div
-						animate={{
-							//jump with spring every 2 seconds
-							y: [0, -5, 0],
-							transition: {
-								duration: 0.3,
-								repeat: Infinity,
-								repeatDelay: 2,
-							},
-						}}
-					>
-						<a href="https://www.eventbrite.it/e/biglietti-serverlessdays-milano-2024-788514818047" target="_blank">
+					<motion.div>
+						<a
+							href="https://www.eventbrite.it/e/biglietti-serverlessdays-milano-2024-788514818047"
+							target="_blank"
+							className="relative"
+						>
+							<MotionButton
+								className="font-semibold text-white rounded-xl"
+								animate={{
+									//jump with spring every 2 seconds
+									y: [0, -5, 0],
+									transition: {
+										duration: 0.3,
+										repeat: Infinity,
+										repeatDelay: 2,
+									},
+								}}
+							>
 								🎫 Buy Ticket!
+								{/* <span className="absolute rotate-[20deg] translate-x-14 rounded-xl bg-inherit p-1 -translate-y-4 underline">
+									35$
+								</span> */}
+							</MotionButton>
+							<div className="absolute w-20 scale-50 -bottom-12 -right-16">
+								<img
+									src={arrowHandDrawn}
+									alt="hand drawn arrow"
+									className="invert sepia-0 saturate-[7491%] hue-rotate-[354deg] brightness-[103%] contrast-[101%]"
+								/>
+							</div>
+
+							<p className="absolute w-20 text-sm font-bold top-12 -right-24 handwriting-1">Early Bird Ticket Now!</p>
 						</a>
 					</motion.div>
 				</div>
-				<div>
+				<div className="mt-10 md:mt-0">
 					<img src={UnicornMascot} alt="unicorn" className="hidden md:block w-48 md:w-72 scale-x-[-1] " />
 
 					<img
