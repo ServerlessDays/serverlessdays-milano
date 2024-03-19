b7a6955f2047d4ea46dc9a6176cc98196b88db89
Marco Giuseppini <giuseppini.marco@live.it>
2024-03-19 16:28:32 +0100

    fix: number padding on agenda

--- src/Components/Agenda.tsx
+++ src/Components/Agenda.tsx
@@ -108,10 +108,16 @@ const Agenda = ({ talks }: { talks: AgendaItem[] }) => {
 const TalkCard = ({ talk: agendaTalk, index }: { talk: AgendaItem; index: number }) => {
 	if (!agendaTalk || !agendaTalk.agenda_details.type === undefined) return null;
 
+	const pad = (num: any, size: number) => {
+		num = num.toString();
+		while (num.length < size) num = "0" + num;
+		return num;
+	}
+
 	const getTimestamp = (time: string) => {
 		const hours = new Date(time).getHours();
 		const minutes = new Date(time).getMinutes();
-		return `${hours == 0 ? "00" : hours}:${minutes == 0 ? "00" : minutes}`;
+		return `${pad(hours, 2)}:${pad(minutes, 2)}`;
 	};
 
 	const title = agendaTalk?.talk?.title || agendaTalk?.break?.title;
