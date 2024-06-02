53c8d6ef45c6d0b665f2cf70752fefa013b3b86d
Federico Tensi <federicotensi@gmail.com>
2024-06-02 15:08:33 +0200

    chore: Linter settings

---
+++ src/types/agenda.d.ts
@@ -0,0 +1,28 @@
+export interface AgendaDetails {
+  start_time: string;
+  end_time: string;
+  minutes: number;
+  keynote?: boolean;
+  type: 'talk' | 'break';
+}
+
+export interface Talk {
+  title: string;
+  description: string;
+  name: string;
+  avatar: string;
+  url: string;
+  organization: string;
+  job_title: string;
+}
+
+export interface Break {
+  title: string;
+  inline_abstract: string;
+}
+
+export interface AgendaItem {
+  agenda_details: AgendaDetails;
+  talk?: Talk;
+  break?: Break;
+}
